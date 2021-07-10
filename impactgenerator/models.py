f84549227419ded58e1de3426e742f326b295d5f
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-10 13:45:31 +0530

    upload commit

--- impactgenerator/models.py
+++ impactgenerator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Document(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    csv = models.FileField(upload_to="document/csv")
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
