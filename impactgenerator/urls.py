6aef3199d69e31d9337d491816eea4804376c98d
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-11 15:05:52 +0530

    impact document generator added

--- impactgenerator/urls.py
+++ impactgenerator/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('create_impact/<str:pk>/', views.create_impact, name="create_impact"),
     # path('upload/', views.upload, name="upload"),
     # path('document/', views.document, name="document"),
     # path('document/upload/', views.upload_document, name="upload_document")
