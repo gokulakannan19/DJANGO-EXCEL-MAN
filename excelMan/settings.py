285ef53e7477612f581412309c0f85fa7b22fe2c
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-07-12 11:22:48 +0530

    setting file updated for allowed host

--- excelMan/settings.py
+++ excelMan/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&mt8d9^w1jg5fehppokyra$saqq(wrm*+81i^9ci#2er(x&tdz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['impact-generator.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
